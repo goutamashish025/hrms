681446ecc3ca2384cfe5fd78edae552c2163e290
Ashish <goutamashish025@gmail.com>
2026-05-02 00:18:52 +0530

    New resttructure branch

---
+++ hrms-frontend/src/app/attendance/apply-wfh/apply-wfh.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApplyWfh } from './apply-wfh';
+
+describe('ApplyWfh', () => {
+  let component: ApplyWfh;
+  let fixture: ComponentFixture<ApplyWfh>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ApplyWfh]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ApplyWfh);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ hrms-frontend/src/app/attendance/apply-wfh/apply-wfh.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-apply-wfh',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './apply-wfh.html',
+  styleUrl: './apply-wfh.scss'
+})
+export class ApplyWfh {
+
+  wfh: any = {
+    date: '',        // ✅ must match backend
+    type: 'WFH',     // ✅ enum value
+    reason: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    const token = (localStorage.getItem('token') || '').replace(/"/g, '');
+
+    if (!this.wfh.date || !this.wfh.reason) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    this.http.post(
+      'http://localhost:8080/api/employee/work-request',
+      this.wfh,
+      {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        })
+      }
+    ).subscribe({
+      next: (res) => {
+        console.log(res);
+        alert("WFH request submitted ✅");
+
+        // reset form
+        this.wfh = {
+          date: '',
+          type: 'WFH',
+          reason: ''
+        };
+      },
+      error: (err) => {
+        console.error(err);
+        alert("Error submitting request ❌");
+      }
+    });
+  }
+}
